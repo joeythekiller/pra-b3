a8624f8ce13e3be2991dfdf13d7f1b85a559f998
flix <113835743+joeythekiller@users.noreply.github.com>
2025-03-21 09:05:47 +0000

    alles geformateerd

--- website/backend/config.php
+++ website/backend/config.php
@@ -1,11 +1,4 @@
 <?php
-
-//IEDER VOOR ZICH:
-
-#### TODO
-//Hernoem dit bestand naar 'config.php' en vul jouw eigen database-gegevens in.
-//Deze config wordt hierna _niet_ meegestuurd naar je groepsgenoten. Zo kan iedereen zijn eigen wachtwoord, etc. invullen.
-
 $dbHost = 'localhost';
 $dbName = 'login';
 $dbUser = 'root';
